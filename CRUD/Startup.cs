d5225ba832ceb9c3d5076b50334f5a08c8b8c2b9
caiolucasb <caio.lucas75@gmail.com>
2021-05-24 01:10:34 -0300

    mudando minha controller no map route

--- CRUD/Startup.cs
+++ CRUD/Startup.cs
@@ -55,7 +55,7 @@ namespace CRUD
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=User}/{action=Index}/{id?}");
             });
         }
     }
