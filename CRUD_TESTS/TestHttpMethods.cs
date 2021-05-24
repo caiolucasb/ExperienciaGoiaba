b72af2a827cfa836e6c31c1508183b2665db0762
caiolucasb <caio.lucas75@gmail.com>
2021-05-24 13:26:44 -0300

    Alterando nome da classe e dos metodos

---
+++ CRUD_TESTS/TestHttpMethods.cs
@@ -0,0 +1,35 @@
+using System;
+using Xunit;
+using CRUD;
+
+namespace CRUD_TESTS
+{
+    public class TestHttpMethods
+    {
+        [Fact]
+        public void ItWasReturnedTest()
+        {
+
+        }
+        [Fact]
+        public void ItWasListedTest()
+        {
+
+        }
+        [Fact]
+        public void ItWasCreatedTest()
+        {
+
+        }
+        [Fact]
+        public void ItWasEditedTest()
+        {
+
+        }
+        [Fact]
+        public void ItWasDeletedTest()
+        {
+
+        }
+    }
+}
