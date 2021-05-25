f9b47a90553db3da1069d46c9f7d110533fd224f
caiolucasb <caio.lucas75@gmail.com>
2021-05-25 14:51:09 -0300

    Finalizando metodos deletetest e edittest

--- CRUD_TESTS/TestHttpMethods.cs
+++ CRUD_TESTS/TestHttpMethods.cs
@@ -78,12 +78,59 @@ namespace CRUD_TESTS
             mockContext.Verify(m => m.Add(It.IsAny<UserModel>()),Times.Once());
             mockContext.Verify(m => m.SaveChanges(), Times.Once());
         }
-        /*[Fact]
+        [Fact]
         public void ItWasEditedTest()
         {
+            //Criação do contexto e do bdset com mock(Arrange)
+            var data = new List<UserModel>{
+                new UserModel(){
+                id= 5,
+                firstName = "Steven",
+                surname = "Ericksen",
+                age = 40
+                },
+                new UserModel(){
+                id= 6,
+                firstName = "George",
+                surname = "Ericksen",
+                age = 55
+                },
+                new UserModel(){
+                id= 7,
+                firstName = "Patrick",
+                surname = "Ericksen",
+                age = 45
+                }
+            }.AsQueryable();
 
+            var mockSet = new Mock<DbSet<UserModel>>();
+            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<UserModel>>().Setup(m => m.Expression).Returns(data.Expression);
+
+            var mockContext = new Mock<UserContext>();
+            //Criando nosso contexto mockado para passar para nossso argumento do constructor da controller
+            mockContext.Setup(m => m.Users).Returns(mockSet.Object);
+            //Criando classe para instanciar o metodo post que iremos testar
+            var service = new UserController(mockContext.Object);
+            //Usando metodo testavel(Act)
+        
+            service.MetodoPut(
+                new UserModel(){
+                firstName = "PatrickGeorge",
+                surname = "Ericksen",
+                age = 75
+                },6);
+            service.MetodoPut(
+                new UserModel(){
+                firstName = "PatrickAlterado",
+                surname = "Ericksen",
+                age = 30
+                },6);
+            //(Assert)
+            mockContext.Verify(m => m.Update(It.IsAny<UserModel>()),Times.Exactly(2));
+            mockContext.Verify(m => m.SaveChanges(), Times.Exactly(2));
         }
-        */
+        
         [Fact]
         public void ItWasDeletedTest()
         {
